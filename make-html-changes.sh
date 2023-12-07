1c51fdac3a95d65b655e1ac5064415fcdf15491e
Yuki Minamoto <yuki.minamoto@fixstars.com>
2023-12-07 18:54:59 +0900

    ✨ 📝 Fix abbreviation

--- make-html-changes.sh
+++ make-html-changes.sh
@@ -2,7 +2,7 @@
 
 for NBPATH in $(git diff --name-only HEAD^..HEAD | grep '.ipynb'); do
     DIR=html/$(echo ${NBPATH} | cut -d / -f 2-3)/
-    OUTPUT=$(jupyter nbconvert --execute --embed-images --output-dir=$DIR --to html "$NBPATH")
+    OUTPUT=$(jupyter nbconvert --embed-images --output-dir=$DIR --to html "$NBPATH")
     if [ -n "$OUTPUT" ]; then
         echo "[NbConvertApp] Converting notebook $NBPATH to html" 1>&2
         echo "$OUTPUT" 1>&2
--- make-html.sh
+++ make-html.sh
@@ -4,7 +4,7 @@
 # For newer directories
 for NBPATH in $(shopt -s globstar && /bin/ls -1 notebooks/**/tutorials/*.ipynb notebooks/**/examples/*.ipynb); do
     DIR=html/$(echo ${NBPATH} | cut -d / -f 2-3)/
-    OUTPUT=$(jupyter nbconvert --execute --embed-images --output-dir=$DIR --to html "$NBPATH")
+    OUTPUT=$(jupyter nbconvert --embed-images --output-dir=$DIR --to html "$NBPATH")
     if [ -n "$OUTPUT" ]; then
         echo "[NbConvertApp] Converting notebook $NBPATH to html" 1>&2
         echo "$OUTPUT" 1>&2
